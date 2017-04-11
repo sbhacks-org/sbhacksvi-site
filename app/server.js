e1dfdc48d0759b85b1d75065fbe4905164f1d0ff
dannycho7 <dannycho7@gmail.com>
2017-04-11 00:53:09 -0700

    server to be configured with error handling

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 5000;
+const port = ( process.env.PORT || 5000 );
 
 app.set('view engine', 'ejs');
 
@@ -11,6 +11,12 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+// Error handling
+app.get('*', (req, res) => {
+  console.log("Invalid URL processed: ", req.url);
+  res.status(404).render('404', {url: req.url});
 });
 
 app.listen(port, () => {
