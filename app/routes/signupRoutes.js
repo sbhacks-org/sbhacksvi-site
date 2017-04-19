d70869fbd697cb5df199d23eea8e0a5109a6f36c
dannycho7 <dannycho7@gmail.com>
2017-04-19 13:18:41 -0700

    resume upload

--- app/routes/signupRoutes.js
+++ app/routes/signupRoutes.js
@@ -1,32 +1,49 @@
 const router = require('express').Router();
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const fs = require('fs');
+const multer = require('multer');
+const upload = multer({
+  dest: 'tmp/',
+  fileFilter: (req, file, cb) => {
+    if(file.size > 4000000) {
+      return cb(new Error('File size was too big'));
+    }
+    if(file.mimetype != 'application/pdf') {
+      return cb(new Error('File was not a pdf'));
+    }
+    cb(null, true);
+  }
+}).single('resume');
 
 router.get('/', (req, res) => {
   if (req.isAuthenticated()) {
-    res.redirect('/user/content');
+    return res.redirect('/user/content');
   }
-  else {
-    res.render('signup');
-  }
+  res.render('signup');
 });
 
-router.post('/', (req, res) => {
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(req.body.password, salt, function(err, hash) {
-        let newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: hash
-        });
-        newUser.save((err) => {
-          if(err) {
-            console.log(err);
-            res.render('signup', {message: "Unsuccessful on creating an account"});
-          } else {
-            res.render('login', {success: "Successfully created an account"});
-          }
-        });
+router.post('/', (req, res, next) => {
+  upload(req, res, (err) => {
+    if(err) {
+      return res.render('signup', { message: err });
+    }
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(req.body.password, salt, function(err, hash) {
+          let newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash,
+            filename: req.file.filename
+          });
+          newUser.save((err) => {
+            if(err) {
+              console.log(err);
+              return res.render('signup', {message: "Unsuccessful on creating an account"});
+            }
+            return res.render('login', {success: "Successfully created an account"});
+          });
+      });
     });
   });
 });
