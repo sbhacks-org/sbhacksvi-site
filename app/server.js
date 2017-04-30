95e70e2c690c306548e3f81b07cdd042ebb13c48
dannycho7 <dannycho7@gmail.com>
2017-04-29 18:12:00 -0700

    more neat code

--- app/server.js
+++ app/server.js
@@ -5,35 +5,17 @@ global.Promise = require('bluebird');
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = ( process.env.PORT || 5000 );
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const passport = require('./passport-setup');
 const logger = require('morgan');
 const favicon = require('serve-favicon');
 
-// Connecting to postgres database
-const models = require('./models/index');
-models.sequelize.sync({
-    force: true // Temporary for development
-  }).then(() => {
-    console.log("Successfully migrated and connected to database");
-
-    // Insert into schools table a default entry for now
-    models.school.create({
-      name: "UC Santa Barbara"
-    }).then(() => {
-      console.log("Successfully added UC Santa Barbara to schools table");
-    }).catch((err) => {
-      console.log("Could not insert into schools table the value UC Santa Barbara");
-    });
-
-    app.listen(port, () => {
-      console.log('Server listening in on port', port);
-    });
-  }).catch((err) => {
-    console.log("Could not connect to database", err);
-  });
+/*
+ * sequelize-db-setup also makes app listen on port
+ * returns instance of models object
+ */
+const models = require('./sequelize-setup')(app);
 
 
 // Middleware
@@ -41,7 +23,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(favicon(path.join(__dirname, 'static/images', 'favicon.ico')));
 app.use(logger('dev'));
 
@@ -57,24 +39,5 @@ app.use(session(
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-/*
- * Routes defined here
- */
-const defaultRoutes = require(path.join(__dirname,'routes/defaultRoutes'));
-const signupRoutes = require(path.join(__dirname, 'routes/signupRoutes'));
-const userRoutes = require(path.join(__dirname, 'routes/userRoutes'));
-
-app.use('/', defaultRoutes);
-app.use('/signup', signupRoutes);
-app.use('/user', userRoutes);
-
-/*
- * End Routes Definitions
- */
-
-// Somewhat Error handling for development purposes
-app.get('*', (req, res) => {
-  console.log("Invalid URL processed: ", req.url);
-  res.status(404).render('404', {url: req.url});
-});
+// Call main route controller here
+require('./routes/controller')(app);
