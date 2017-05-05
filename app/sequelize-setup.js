04f2270034eb28c45845177159760d46c42e9eea
dannycho7 <dannycho7@gmail.com>
2017-05-05 02:42:08 -0700

    comment out force

--- app/sequelize-setup.js
+++ app/sequelize-setup.js
@@ -4,7 +4,7 @@ const port = ( process.env.PORT || 5000 );
 module.exports = (app) => {
   // Connecting to postgres database
   models.sequelize.sync({
-      force: true // Temporary for development
+      // force: true // Temporary for development
     }).then(() => {
       console.log("Successfully migrated and connected to database");
 
@@ -20,6 +20,7 @@ module.exports = (app) => {
       app.listen(port, () => {
         console.log('Server listening in on port', port);
       });
+
     }).catch((err) => {
       console.log("Could not connect to database", err);
     });
