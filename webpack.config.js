ca0d754c2e062ff6e977605d69d2dd6c9600c233
dannycho7 <dannycho7@gmail.com>
2017-05-07 20:41:07 -0700

    simple webpack config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/components/main.js',
+  output: {
+    path: path.join(__dirname, 'src/static/js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
