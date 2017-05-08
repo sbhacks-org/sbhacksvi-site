81b478659e601ec0a0f80da234bf72019a57faa8
dannycho7 <dannycho7@gmail.com>
2017-05-07 19:35:02 -0700

    Bin folder exec

---
+++ src/bin/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+global.Promise = require('bluebird');
+
+const models = require('../models/index');
+const port = ( process.env.PORT || 5000 );
+const express = require('express');
+const app = express();
+
+require('../app')(app);
+
+// Connecting to postgres database
+models.sequelize.sync({
+  // force: true // Temporary for development
+}).then(() => {
+  console.log("Successfully migrated and connected to database");
+
+  // Insert into schools table a default entry for now
+  models.school.create({
+    name: "UC Santa Barbara"
+  }).then(() => {
+    console.log("Successfully added UC Santa Barbara to schools table");
+  }).catch((err) => {
+    console.log("Could not insert into schools table the value UC Santa Barbara");
+  });
+
+  app.listen(port, () => {
+    console.log('Server listening in on port', port);
+  });
+
+}).catch((err) => {
+  console.log("Could not connect to database", err);
+});
