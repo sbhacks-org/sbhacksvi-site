47e1d08f3d6e95241e75d92432ac7d64c41f86cf
dannycho7 <dannycho7@gmail.com>
2017-05-07 19:35:18 -0700

    rename to app.js with server setup tooling

---
+++ src/app.js
@@ -0,0 +1,34 @@
+// Server Dependency Setup
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const passport = require('./config/passport-setup')();
+const logger = require('morgan');
+const favicon = require('serve-favicon');
+
+module.exports = (app) => {
+  // Middleware
+  app.set('view engine', 'ejs');
+  app.set('views', path.join(__dirname, 'views'));
+  app.use(express.static(path.join(__dirname, 'static')));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(favicon(path.join(__dirname, 'static/images', 'favicon.ico')));
+  app.use(logger('dev'));
+
+  app.use(session(
+    {
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false
+    })
+  );
+
+  // Passport initialize
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // Call main route controller here
+  require('./routes/controller')(app);
+}
