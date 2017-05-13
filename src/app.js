4eca61837f8abecd4b520a9babba1c0d8211e744
dannycho7 <dannycho7@gmail.com>
2017-05-13 00:43:16 -0700

    Helmet.js added for security

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const passport = require('./config/passport-setup')();
 const logger = require('morgan');
+const helmet = require('helmet');
 const favicon = require('serve-favicon');
 
 module.exports = (app) => {
@@ -15,6 +16,7 @@ module.exports = (app) => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(favicon(path.join(__dirname, 'static/images', 'favicon.ico')));
+  app.use(helmet());
   app.use(logger('dev'));
 
   app.use(session(
