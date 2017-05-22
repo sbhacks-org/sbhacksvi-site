e0db23504b60e366041dd938684ae7e7148accda
dannycho7 <dannycho7@gmail.com>
2017-05-21 22:23:03 -0700

    add migration for school

---
+++ src/migrations/schools.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    let schools = [
+      { name: 'UC Berkeley' },
+      { name: 'UC Davis' },
+      { name: 'UC Los Angeles' },
+      { name: 'UC Merced' },
+      { name: 'UC Riverside' },
+      { name: 'UC Santa Barbara' },
+      { name: 'UC Santa Cruz' },
+      { name: 'UC San Diego' },
+      { name: 'UC Irvine'},
+      { name: 'Cal Poly SLO' },
+      { name: 'Cal Poly Pomona' },
+      { name: 'Santa Barbara City College' },
+      { name: 'SF State University' },
+      { name: 'Stanford University' },
+      { name: 'California Institute of Technology (Caltech)' },
+      { name: 'CSU Long Beach' },
+      { name: 'Carnegie Mellon University' },
+      { name: 'University of Southern California' }
+    ];
+    
+    return queryInterface.bulkInsert('schools', schools);
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
