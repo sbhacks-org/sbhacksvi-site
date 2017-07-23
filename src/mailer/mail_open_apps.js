f9f783b256efb89a4191f5b44a40fc319a7bc6ba
dannycho7 <dannycho7@gmail.com>
2017-07-23 12:19:19 -0700

    open_apps mailer updates notification column to avoid duplicate mailing + sends to all subscribers

--- src/mailer/mail_open_apps.js
+++ src/mailer/mail_open_apps.js
@@ -1,27 +1,31 @@
 #!/usr/bin/env node
 const transporter = require("./transporter");
 const renderTemplate = require("./renderTemplate");
+const Subscriber = require("../models").subscriber;
 
-let renderText = renderTemplate("open_apps.txt.ejs", {
-	name: "Danny Cho"
-});
-let renderHTML = renderTemplate("open_apps.ejs", {
-	name: "Danny Cho"
-});
+Subscriber.findAll({})
+.then((subscribers) => {
+	subscribers.forEach((subscriber) => {
+		if(subscriber.notified == true) return;
+		let renderText = renderTemplate("open_apps.txt.ejs");
+		let renderHTML = renderTemplate("open_apps.ejs");
+		Promise.all([renderText, renderHTML])
+		.then((content) => {
+			const message = {
+				to: subscriber.email,
+				subject: "SB Hacks IV Applications are OPEN!!",
+				text: content[0],
+				html: content[1]
+			}
 
-Promise.all([renderText, renderHTML])
-.then((content) => {
-	const message = {
-		to: "dannycho7@gmail.com",
-		subject: "SB Hacks IV Applications are OPEN!!",
-		text: content[0],
-		html: content[1]
-	}
-
-	transporter.sendMail(message, (err, info) => {
-		if(err) throw err;	
-		console.log("Message successfully sent");
-		console.log(info);
-		transporter.close();
+			transporter.sendMail(message, (err, info) => {
+				if(err) console.log(err);	
+				console.log(info);
+				subscriber.update({ notified: true }).then(() => transporter.close());
+			});
+		});
 	});
+})
+.catch((err) => {
+	throw err;
 });
