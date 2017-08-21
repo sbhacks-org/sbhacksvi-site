70d294fedf30426710bc33cc6056e062b3509201
dannycho7 <dannycho7@gmail.com>
2017-08-21 00:19:31 -0700

    Configure dashboard flow to link to application flow

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 const isLoggedIn = require("../lib/isLoggedIn");
 
-router.get("/dashboard", isLoggedIn, (req, res) => res.render("dashboard"));
+router.get("/dashboard", isLoggedIn, (req, res) => {
+	req.user.getApplication().then((application) => {
+		res.locals.application = application;
+		res.render("dashboard");
+	});
+});
 
 router.use("/update", (err, req, res, next) => {
 	throw err; // temporary
