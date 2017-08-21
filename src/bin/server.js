141884722bdf3659dfada6e3c815ac1709162706
dannycho7 <dannycho7@gmail.com>
2017-08-20 17:48:39 -0700

    Reformat of file naming conventions

--- src/bin/server.js
+++ src/bin/server.js
@@ -3,22 +3,15 @@ global.Promise = require("bluebird");
 
 const models = require("../models/index");
 const port = ( process.env.PORT || 5000 );
-const express = require("express");
-const app = express();
+const app = require("../app");
 
-// importing all changes to app object
-require("../app")(app);
-
-// Connecting to postgres database
 models.sequelize.sync({
 	logging: false
 }).then(() => {
 	console.log("Successfully migrated and connected to database");
-
 	app.listen(port, () => {
 		console.log("Server listening in on port", port);
 	});
-
 }).catch((err) => {
 	console.log("Could not connect to database", err);
 });
