4c7f3c740c18972d6953766b76062f2b757a63cd
dannycho7 <dannycho7@gmail.com>
2017-08-27 20:57:16 -0700

    Move application fields massage function to own module

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,22 +1,4 @@
-const populateWithApplicationFields = (application) => {
-	if(!application) {
-		return undefined;
-	}
-
-	return {
-		school_id: application.school_id || "",
-		level_of_study: application.level_of_study || "",
-		graduation_year: application.graduation_year || "",
-		github: application.github || "",
-		linkedin: application.linkedin || "",
-		major: application.major || "",
-		gender: application.gender || "",
-		phone_number: application.phone_number || "",
-		shirt_size: application.shirt_size || "",
-		transportation: application.transportation || "",
-		resume_url: application.resume_url
-	}
-};
+const populateWithApplicationFields = require("./populateWithApplicationFields");
 
 module.exports.authSuccessUserState = (user, application) => {
 	let user_info = {
