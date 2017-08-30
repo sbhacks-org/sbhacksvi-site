2b39217e5d625f978efcb0a002deec057c90d59e
dannycho7 <dannycho7@gmail.com>
2017-08-29 20:47:33 -0700

    Remove default application save null due to client sanitization

--- src/lib/application.js
+++ src/lib/application.js
@@ -6,20 +6,22 @@ const { School, User, Application } = require("../models");
 module.exports.saveApplication = (req) => {
 	const User = req.user;
 
+	console.log(req.body);
+
 	return User.createApplication({
 		user_id: req.user.id,
-		school_id: req.body.school_id || undefined,
-		resume_url: req.files.resume.Location || null,
-		resume_key: req.files.resume.key || null,
-		transportation: req.body.transportation || null,
-		graduation_year: req.body.graduation_year || null,
-		level_of_study: req.body.level_of_study || null,
-		github: req.body.github || null,
-		linkedin: req.body.linkedin || null,
-		major: req.body.major || null,
-		phone_number: req.body.phone_number || null,
-		shirt_size: req.body.shirt_size || null,
-		gender: req.body.gender || null
+		school_id: req.body.school_id,
+		resume_url: req.files.resume.Location,
+		resume_key: req.files.resume.key,
+		transportation: req.body.transportation,
+		graduation_year: req.body.graduation_year,
+		level_of_study: req.body.level_of_study,
+		github: req.body.github,
+		linkedin: req.body.linkedin,
+		major: req.body.major,
+		phone_number: req.body.phone_number,
+		shirt_size: req.body.shirt_size,
+		gender: req.body.gender
 	});
 };
 
