e2d516215173f5e8e4c54a2999fb8fd377632054
Danny Cho <danny.cho@appfolio.com>
2017-09-04 01:03:26 -0700

    Remove console log of subscriber endpoint

--- src/routes/subscriber.js
+++ src/routes/subscriber.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const subscriberMethods = require("../lib/subscriber");
 
 router.post("/", (req, res) => {
-	console.log(req.body);
 	subscriberMethods.saveSubscriber(req, res);
 });
 
