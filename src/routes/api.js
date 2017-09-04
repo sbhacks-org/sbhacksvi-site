6a28b6664adad4072b590a39d71b9c4cf20c183d
Danny Cho <danny.cho@appfolio.com>
2017-09-03 19:01:56 -0700

    Allow schools list to be generated to include a certain school

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,16 +1,20 @@
 const router = require("express").Router();
 const { School, sequelize } = require("../models");
 
-let school_query =
-`SELECT schools.*, COUNT(applications.id) as application_count
-FROM schools
-LEFT JOIN applications ON schools.id = applications.school_id
-GROUP BY schools.id
-ORDER BY application_count DESC
-LIMIT 25;`;
+let school_query = (include_sql) => {
+	return `SELECT schools.*, COUNT(applications.id) as application_count
+		FROM schools
+		LEFT JOIN applications ON schools.id = applications.school_id
+		GROUP BY schools.id
+		ORDER BY ${include_sql} application_count DESC
+		LIMIT 5;`;
+}
 
 router.get("/schools", (req, res) => {
-	sequelize.query(school_query, { type: sequelize.QueryTypes.SELECT })
+	let { include } = req.query;
+	let include_sql = isNaN(include) ? "" : `schools.id = ${include} DESC,`;
+
+	sequelize.query(school_query(include_sql), { type: sequelize.QueryTypes.SELECT })
 	.then((schools) => {
 		res.json(schools);
 	});
