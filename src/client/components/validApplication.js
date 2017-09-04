dbdd37f3191b28984555208a9543cd2e34300dd1
Danny Cho <danny.cho@appfolio.com>
2017-09-04 01:02:09 -0700

    Creation of validApplication helper which validates text fields for required fields. Also contains resume validation

---
+++ src/client/components/validApplication.js
@@ -0,0 +1,19 @@
+export const invalidResume = (resume) => {
+	return !resume || resume.size > 4194304 || resume.type !== "application/pdf";
+}
+
+export const isValidBasedOnTextFields = (fields) => {
+	let required_fields = [
+		fields.school_id,
+		fields.level_of_study,
+		fields.graduation_year,
+		fields.major,
+		fields.shirt_size,
+		fields.transportation
+	];
+
+	if(required_fields.includes("") || fields.dietary_restrictions.length === 0) {
+		return false;
+	}
+	return true;
+};
