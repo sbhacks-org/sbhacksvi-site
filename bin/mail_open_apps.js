1513481757fad05f9fa3da5b6712d057f89a8edb
dannycho7 <dannycho7@gmail.com>
2017-10-13 23:33:54 -0700

    Move render templates outside of loop because it is static

--- bin/mail_open_apps.js
+++ bin/mail_open_apps.js
@@ -7,14 +7,16 @@ const transporter = require(path.join(mail_path, "transporter"));
 const renderTemplate = require(path.join(mail_path, "renderTemplate"));
 const { Subscriber } = require("../src/models");
 
-Subscriber.findAll({})
-.then((subscribers) => {
-	subscribers.forEach((subscriber) => {
-		if(subscriber.notified == true) return;
-		let renderText = renderTemplate("open_apps.txt.ejs");
-		let renderHTML = renderTemplate("open_apps.ejs");
-		Promise.all([renderText, renderHTML])
-		.then((content) => {
+let renderText = renderTemplate("open_apps.txt.ejs");
+let renderHTML = renderTemplate("open_apps.ejs");
+
+Promise.all([renderText, renderHTML])
+.then((content) => {
+	Subscriber.findAll({})
+	.then((subscribers) => {
+		subscribers.forEach((subscriber) => {
+			// if(subscriber.notified == true) return;
+			
 			const message = {
 				to: subscriber.email,
 				subject: "SB Hacks IV Applications are OPEN!!",
@@ -28,8 +30,8 @@ Subscriber.findAll({})
 				subscriber.update({ notified: true }).then(() => transporter.close());
 			});
 		});
+	})
+	.catch((err) => {
+		throw err;
 	});
-})
-.catch((err) => {
-	throw err;
 });
