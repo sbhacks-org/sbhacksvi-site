b59a079afa40e415add67e0105f24baf2010ad72
dannycho7 <dannycho7@gmail.com>
2017-11-13 23:44:53 -0800

    Add Header component as all route layout

---
+++ src/client/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { logout } from "../../actions";
+import { Button } from "semantic-ui-react";
+
+const mapStateToProps = (state) => {
+	const { isAuthenticated } = state.user;
+
+	return {
+		isAuthenticated
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return bindActionCreators({ logout }, dispatch);
+};
+
+const Header = ({ logout, isAuthenticated }) => {
+	return (
+		<div id="header">
+			<a id="logo" href="/">
+				<img src="/images/logo.png" />
+			</a>
+			{isAuthenticated ?
+				<a id="logout-btn" onClick={logout}>
+					<Button color="teal" fluid size="large">Logout</Button>
+				</a>
+			: null}
+		</div>
+	);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
