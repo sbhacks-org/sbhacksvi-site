bd8e3a6fe32c468ec58fd18f8026b1c581321b17
dannycho7 <dannycho7@gmail.com>
2017-11-23 03:45:31 -0800

    Initialize archive folder
    Moving items:
     - Open apps script
     - Open apps mailing templates
     - Subscriber route file

---
+++ archive/subscriber.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const subscriberMethods = require("../lib/subscriber");
+
+router.post("/", (req, res) => {
+	subscriberMethods.saveSubscriber(req, res);
+});
+
+module.exports = router;
