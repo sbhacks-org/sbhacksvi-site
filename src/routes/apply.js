2c1f8900dbc433750c5d648aa575c922c2953c52
dannycho7 <dannycho7@gmail.com>
2017-11-22 21:28:53 -0800

    Update apply flow for new client side upload support

--- src/routes/apply.js
+++ src/routes/apply.js
@@ -6,8 +6,8 @@ const signupMail = require("../mailer/mail_signup_success");
 const isLoggedIn = require("../lib/isLoggedIn");
 const { saveApplication, formPostUpload, populateWithApplicationFields } = require("../lib/application");
 
-router.post("/", isLoggedIn, formPostUpload.middleware(), (req, res, next) => {
-	saveApplication(req.user, req.files, req.body)
+router.post("/", isLoggedIn, (req, res, next) => {
+	saveApplication(req.user, req.body)
 	.then((application) => {
 		signupMail.send(req.user);
 		return res.json({ success: true, application: populateWithApplicationFields(application) });
