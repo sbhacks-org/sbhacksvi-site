268495610df9b54d036692e3d355e562db10e281
dannycho7 <dannycho7@gmail.com>
2017-11-22 16:45:17 -0800

    Update isLoggedIn middleware to send 403 status code

--- src/lib/isLoggedIn.js
+++ src/lib/isLoggedIn.js
@@ -2,5 +2,6 @@ module.exports = (req, res, next) => {
 	if (req.isAuthenticated()){
 		return next();
 	}
-	next(new Error("You do not hav access to this page."));
+	res.status(403);
+	next(new Error("You do not have access to this page."));
 };
