367db94c5581cbb38005f6014e2378389f2f9429
dannycho7 <dannycho7@gmail.com>
2017-11-23 18:48:07 -0800

    Configure build flow to remove redux-logger in production

--- src/client/store.js
+++ src/client/store.js
@@ -8,4 +8,10 @@ let initialState = window.__PRELOADED_STATE__ || undefined;
 
 delete window.__PRELOADED_STATE__;
 
-module.exports = createStore(reducers, initialState, applyMiddleware(thunk, logger));
+let middleware = [thunk];
+
+if(process.env.NODE_ENV !== "production") {
+	middleware = [...middleware, logger]
+}
+
+module.exports = createStore(reducers, initialState, applyMiddleware(...middleware));
