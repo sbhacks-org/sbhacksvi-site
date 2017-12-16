559194d90636c4187a3872d63b8d0b64ec3e8b5d
dannycho7 <dannycho7@gmail.com>
2017-12-15 21:49:48 -0800

    Add /profile/reset-password route
    Adds token and expiry date on user model

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,5 +1,7 @@
+const crypto = require("crypto");
 const router = require("express").Router();
 
+const { User } = require("../models");
 const isLoggedIn = require("../lib/isLoggedIn");
 const {
 	massageAttrsForUpdate,
@@ -25,6 +27,38 @@ router.post("/update", isLoggedIn, (req, res, next) => {
 		})
 		.catch((err) => next(err));
 	});	
+});
+
+router.post("/reset-password", (req, res, next) => {
+	User.findOne({ where: { email: req.body.email } })
+	.then((user) => {
+		if(!user) {
+			res.json({
+				success: false,
+				message: {
+					type: "success",
+					header: "Invalid Email",
+					content: "No such user with that email exists."
+				}
+			});
+		} else {
+			user.updateAttributes({
+				passwordResetToken: crypto.randomBytes(20).toString("hex"),
+				passwordResetTokenExpires: Date.now() + 60 * 60 * 1000
+			})
+			.then((user) => {
+				res.json({
+					success: true,
+					message: {
+						type: "success",
+						header: "Token Sent",
+						content: "A reset link has been sent to your email"
+					}
+				});
+			});
+		}		
+	})
+	.catch(next);
 });
 
 module.exports = router;
