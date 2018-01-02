b489aed6d63dda9361c02f56f9056a1af60d70e7
dannycho7 <dannycho7@gmail.com>
2018-01-01 21:27:05 -0800

    Change RSVP deadline to Jan 12th

--- src/client/components/Profile/presenters/ApplicationResultView.js
+++ src/client/components/Profile/presenters/ApplicationResultView.js
@@ -7,9 +7,9 @@ const ApplicationResultView = ({ user, application, rsvpAction }) => {
 			<div>
 				<h1>Congratulations, {user.first_name}!</h1>
 				<div className="body">
-					<p className="body-text">We are thrilled to invite you to SB Hacks IV! We were impressed by your application and can't wait for you to create something extraordinary at the UC Santa Barbara this January 19-21.</p>
+					<p className="body-text">We are thrilled to invite you to SB Hacks IV! We were impressed by your application and can't wait for you to create something extraordinary at UC Santa Barbara this January 19-21.</p>
 					<p className="body-text">Information about buses and travel reimbursement will be emailed out to you soon.</p>
-					<p className="body-text">To confirm your registration and save your spot, take a minute to RSVP no later than Sunday, January 14th, 11:59 PM</p>
+					<p className="body-text">We received an overwhelming 2100+ applications this year for our limited 500 spots. To confirm your registration and save your spot, take a minute to RSVP no later than Friday, January 12th, 11:59 PM.</p>
 				</div>
 				{
 					application.rsvp ?
@@ -24,7 +24,7 @@ const ApplicationResultView = ({ user, application, rsvpAction }) => {
 		<div>
 			<h1>Application Status: <strong>In Review</strong></h1>
 			<div className="body">
-				<p className="body-text">Hello! We are currently reviewing your application. If you don't see an update by January 3rd, please email us at <a href="mailto:team@sbhacks.com">team@sbhacks.com</a></p>
+				<p className="body-text">Hello! We are currently reviewing your application. If you don't see an update by January 12th, please email us at <a href="mailto:team@sbhacks.com">team@sbhacks.com</a></p>
 			</div>
 		</div>
 	);
