7c6c5b8b6603ae761ace46854dabb458164cfd02
dannycho7 <dannycho7@gmail.com>
2018-01-02 21:56:32 -0800

    Add rsvp mailer that mails part 1 of logistics

---
+++ src/mailer/mail_rsvp.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+const sgMail = require("@sendgrid/mail");
+const renderTemplate = require("./renderTemplate");
+
+sgMail.setApiKey(process.env.SENDGRID_KEY);
+
+module.exports.send = (user) => {
+	let renderText = renderTemplate("rsvp.txt.ejs", user);
+	let renderHTML = renderTemplate("rsvp.ejs", user);
+
+	Promise.all([renderText, renderHTML])
+	.then((content) => {
+		const message = {
+			to: user.email,
+			from: "SB Hacks <team@sbhacks.com>",
+			subject: "SB Hacks IV Event Logistics Part 1",
+			text: content[0],
+			html: content[1]
+		};
+
+		sgMail.send(message);
+	});
+};
