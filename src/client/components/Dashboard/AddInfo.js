384688d4bbcfd34b47fee23ac8f4136da32e0e84
jenniferlai43 <jenniferlai43@gmail.com>
2018-12-23 14:34:57 -0800

    Dashboard added RSVP action to accepted/non accepted message

--- src/client/components/Dashboard/AddInfo.js
+++ src/client/components/Dashboard/AddInfo.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Container } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-const AddInfo = ({info, application}) => {
+const AddInfo = ({info, application, rsvpAction}) => {
 
 	if (application)
 	{
@@ -12,10 +12,15 @@ const AddInfo = ({info, application}) => {
 				(<div className="body">
 					<p className="body-text">Congratulations, {info.first_name}! We are thrilled to invite you to SB Hacks V! We were impressed by your application and can't wait for you to create something extraordinary at UC Santa Barbara this January 11-13.</p>
 					<p className="body-text">Information about buses and travel reimbursement will be emailed out to you soon.</p>
-					<p className="body-text">We received an overwhelming 2,100+ applications this year for our limited 500 spots. To confirm your registration and save your spot, take a minute to <strong>RSVP</strong> no later than <strong>Friday, January 4th, 11:59 PM</strong>.</p>
+					<p className="body-text">We received an overwhelming 1,700+ applications this year for our limited 500 spots. To confirm your registration and save your spot, take a minute to <strong>RSVP</strong> no later than <strong>Friday, January 4th, 11:59 PM</strong>.</p>
+				{
+					application.rsvp ?
+						<p className="body-text">Your <strong>RSVP</strong> has been noted. We look forward to seeing you at SB Hacks!</p> :
+						<Button fluid color="teal" onClick={rsvpAction}>RSVP</Button>
+				}
 				</div>) : 
 				(<div className="body">
-					<p className="body-text">Unfortunately, we are unable to offer you admission to SB Hacks V. We had an overwhelming 2,100+ applications this year for our limited 500 spots. We encourage you to apply next year. Best of luck!</p>
+					<p className="body-text">Unfortunately, we are unable to offer you admission to SB Hacks V. We had an overwhelming 1,700+ applications this year for our limited 500 spots. We encourage you to apply next year. Best of luck!</p>
 				</div>)
 		}
 		else
