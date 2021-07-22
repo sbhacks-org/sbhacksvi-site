7ac9d7f2c97236d64ef5f6adf67903c89e1652f4
Ben1152000 <benjamindarnell00@gmail.com>
2021-07-21 22:21:04 -0700

    Remove dependency on databases

--- bin/server.js
+++ bin/server.js
@@ -6,12 +6,16 @@ const app = require("../src/app");
 const port = ( process.env.PORT || 5000 );
 const http = require("http");
 
-models.sequelize.sync({
-	logging: app.get("env") !== "production" && console.log,
-	force: false
-}).then(() => {
-	console.log("Successfully migrated and connected to database");
-	http.createServer(app).listen(port, () => console.log("Server listening in on port", port));
-}).catch((err) => {
-	console.log("Could not connect to database", err);
-});
+// models.sequelize.sync({
+// 	logging: app.get("env") !== "production" && console.log,
+// 	force: false
+// }).then(() => {
+// 	console.log("Successfully migrated and connected to database");
+// 	http.createServer(app).listen(port, () => console.log("Server listening in on port", port));
+// }).catch((err) => {
+// 	console.log("Could not connect to database", err);
+// });
+
+// Ben: I removed the lines above so that the site will work without the database.
+
+http.createServer(app).listen(port, () => console.log("Server listening in on port", port));
